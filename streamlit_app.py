2e73cdfb8b05a9f7a32b3e634511040c48509d3c
Tabeen Mahek Junedi <mahekjunedi10@gmail.com>
2025-06-10 13:08:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
+from fabric_recommender_rule import recommend_fabric_rule
 from fabric_recommender_ml import recommend_fabric_ml
 
-st.title("🧵 ML-Based Fabric Recommender")
+st.title("🧵 Hybrid Fabric Recommender")
 
 garment = st.selectbox("Select Garment Type", ["Shirt", "Pants", "Dress", "Jacket"])
 season = st.selectbox("Select Season", ["Summer", "Winter"])
 style = st.selectbox("Select Style", ["Casual", "Formal"])
+mode = st.radio("Choose Recommendation Mode", ["Rule-Based", "Machine Learning (ML)"])
 
 if st.button("Recommend Fabric"):
-    result = recommend_fabric_ml(garment, season, style)
+    if mode == "Rule-Based":
+        result = recommend_fabric_rule(garment, season, style)
+    else:
+        result = recommend_fabric_ml(garment, season, style)
     st.success(result)
